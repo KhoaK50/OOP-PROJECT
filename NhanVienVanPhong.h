54f9e9c248b3fced27c61d6c5570863072d9da32
nguyen-ngoc-hong-an5001103002 <nguyenngochongan00@gmail.com>
2025-11-13 22:38:29 +0700

    NhanVienVanPhong.h

--- NhanVienVanPhong.h
+++ NhanVienVanPhong.h
@@ -3,26 +3,44 @@
 #include<iostream> 
 #include<iomanip> 
 #include<string> 
+
 using namespace std;
 class NhanVienVanPhong : public NhanVien
 {
 protected: 
-    int SoNgayLamViec;
-    double DonGiaNgay;
+    int SoNgayLamViec;//số ngày làm việc bình thường
+    int SoNgayLe;//số ngày làm việc Lễ, Tết
+    double DonGiaNgay;//Đơn giá ngày thường 
+    double PhuCap;//Phụ cấp (có thể 0 nếu không có)
+    double GioLamThemNgayThuong;//Giờ làm thêm ngày thường
+    double GioLamThemCuoiTuan;//Giờ làm thêm cuối tuần
+    double GioLamThemLeTet;//Giờ làm thêm ngày Lễ, Tết 
 public:
     NhanVienVanPhong();
-    NhanVienVanPhong(string _MaNV="", string _HoTen="", string _NgaySinh="", string _DiaChi="", int _SoNgay=0,double _DonGia=0);
+    NhanVienVanPhong(string _MaNV, string _HoTen, string _NgaySinh, string _DiaChi, int _SoNgayLamViec = 0, int _SoNgayLe = 0,
+         double _DonGiaNgay = 0, double _PhuCap = 0, double _GioLamThemNgayThuong = 0, double _GioLamThemCuoiTuan = 0, double _GioLamThemLeTet = 0);
     NhanVienVanPhong(const NhanVienVanPhong& nv);
     ~NhanVienVanPhong();
     
     int getSoNgayLamViec() const;
+    int getSoNgayLe() const;
     double getDonGiaNgay() const;
+    double getPhuCap() const;
+    double getGioLamThemNgayThuong() const;
+    double getGioLamThemCuoiTuan() const;
+    double getGioLamThemLeTet() const;
+
     void setSoNgayLamViec(int);
+    void setSoNgayLe(int);
     void setDonGiaNgay(double);
+    void setPhuCap(double);
+    void setGioLamThemNgayThuong(double);
+    void setGioLamThemCuoiTuan(double);
+    void setGioLamThemLeTet(double);
 
     virtual double TinhLuong() const;
     virtual void HienThiThongTin() const;
 
     friend istream& operator>>(istream& in, NhanVienVanPhong& nv);
     friend ostream& operator<<(ostream& out, const NhanVienVanPhong& nv);
-};
+}; 
